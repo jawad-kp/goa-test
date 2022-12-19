072fac2a068e97d692298976dfffdfad39358a24
Jawad Ahmed Khan <studboii@users.noreply.github.com>
2022-12-19 09:19:25 +0530

    feature(design): added design files

---
+++ design/design.go
@@ -0,0 +1,87 @@
+package design
+
+import (
+	. "goa.design/goa/v3/dsl"
+)
+
+var _ = API("calc", func() {
+	Title("Calculator Service")
+	Description("Service for multiplying numbers, a Goa teaser")
+	Server("calc", func() {
+		Host("localhost", func() {
+			URI("http://localhost:8000")
+			URI("grpc://localhost:8080")
+		})
+	})
+})
+
+var _ = Service("calc", func() {
+	Description("The calc service performs operations on numbers.")
+
+	Method("multiply", func() {
+		Payload(func() {
+			Field(1, "a", Int, "Left operand")
+			Field(2, "b", Int, "Right operand")
+			Required("a", "b")
+		})
+
+		Result(Int)
+
+		HTTP(func() {
+			GET("/multiply/{a}/{b}")
+		})
+
+		GRPC(func() {
+		})
+	})
+	Method("add", func() {
+		Payload(func() {
+			Field(1, "a", Int, "Left operand")
+			Field(2, "b", Int, "Right operand")
+			Required("a", "b")
+		})
+
+		Result(Int)
+
+		HTTP(func() {
+			GET("/add/{a}/{b}")
+		})
+
+		GRPC(func() {
+		})
+	})
+	Method("subtract", func() {
+		Payload(func() {
+			Field(1, "a", Int, "Left operand")
+			Field(2, "b", Int, "Right operand")
+			Required("a", "b")
+		})
+
+		Result(Int)
+
+		HTTP(func() {
+			GET("/subtract/{a}/{b}")
+		})
+
+		GRPC(func() {
+		})
+	})
+	Method("divide", func() {
+		Payload(func() {
+			Field(1, "a", Int, "Left operand")
+			Field(2, "b", Int, "Right operand")
+			Required("a", "b")
+		})
+
+		Result(Float64)
+
+		HTTP(func() {
+			GET("/divide/{a}/{b}")
+		})
+
+		GRPC(func() {
+		})
+	})
+
+	Files("/openapi.json", "./gen/http/openapi.json")
+})
