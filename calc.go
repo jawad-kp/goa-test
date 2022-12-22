5b532671e4b4dd4a196f41c3af93e469f4e8c184
Jawad Ahmed Khan <42826824+jawad-kp@users.noreply.github.com>
2022-12-22 19:26:04 +0530

    feature(service): updated to be include creation of a new note

--- calc.go
+++ calc.go
@@ -2,55 +2,58 @@ package calcapi
 
 import (
 	"context"
+	"goa-test/db"
 	"goa-test/gen/calc"
+	"goa-test/models"
 	"log"
 )
 
 // calc service example implementation.
 // The example methods log the requests and return zero values.
-type calcsrvc struct {
+type calcService struct {
 	logger *log.Logger
+	db     db.DB
 }
 
 // NewCalc returns the calc service implementation.
-func NewCalc(logger *log.Logger) calc.Service {
-	return &calcsrvc{logger}
+func NewCalc(logger *log.Logger, db db.DB) calc.Service {
+	return &calcService{logger: logger, db: db}
 }
 
 // Multiply implements multiply.
-func (s *calcsrvc) Multiply(_ context.Context, p *calc.MultiplyPayload) (res int, err error) {
+func (s *calcService) Multiply(_ context.Context, p *calc.MultiplyPayload) (res int, err error) {
 	res = p.A * p.B
 	return
 }
 
 // Add implements add.
-func (s *calcsrvc) Add(_ context.Context, p *calc.AddPayload) (res int, err error) {
+func (s *calcService) Add(_ context.Context, p *calc.AddPayload) (res int, err error) {
 	res = p.A + p.B
 	return
 }
 
 // Subtract implements subtract.
-func (s *calcsrvc) Subtract(_ context.Context, p *calc.SubtractPayload) (res int, err error) {
+func (s *calcService) Subtract(_ context.Context, p *calc.SubtractPayload) (res int, err error) {
 	res = p.A - p.B
 	return
 }
 
 // Divide implements divide.
-func (s *calcsrvc) Divide(_ context.Context, p *calc.DividePayload) (res float64, err error) {
+func (s *calcService) Divide(_ context.Context, p *calc.DividePayload) (res float64, err error) {
 	res = float64(p.A) / float64(p.B)
 	return
 }
 
 // GetNotes implements getNotes.
-func (s *calcsrvc) GetNotes(_ context.Context, _ *calc.GetNotesPayload) (res *calc.GetNotesResult, err error) {
+func (s *calcService) GetNotes(_ context.Context, _ *calc.GetNotesPayload) (res *calc.GetNotesResult, err error) {
 	res = &calc.GetNotesResult{}
 	s.logger.Print("calc.getNotes")
 	return
 }
 
-// CreateNote implements createNote.
-func (s *calcsrvc) CreateNote(_ context.Context, _ *calc.CreateNotePayload) (res *calc.Note, err error) {
-	res = &calc.Note{}
+func (s *calcService) CreateNote(ctx context.Context, payload *calc.CreateNotePayload) (err error) {
+	noteModel := models.GetNewNote(*payload)
 	s.logger.Print("calc.createNote")
+	noteModel, err = s.db.SaveNoteToDb(ctx, &noteModel)
 	return
 }
