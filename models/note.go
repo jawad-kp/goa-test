929a974d73f4f90063be13c47b4cbd9a2364a25a
Jawad Ahmed Khan <42826824+jawad-kp@users.noreply.github.com>
2022-12-22 19:24:15 +0530

    feature(models/note): added note model

---
+++ models/note.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"goa-test/gen/calc"
+	"time"
+)
+
+type Note struct {
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	DeletedAt time.Time `json:"deleted_at" bson:"deleted_at"`
+	UserID    string    `json:"user_id" bson:"user_id"`
+	Title     string    `json:"title" bson:"title"`
+	Body      string    `json:"body,omitempty" bson:"body"`
+}
+
+func (nt *Note) MarshalBSON() ([]byte, error) {
+	if nt.CreatedAt.IsZero() {
+		nt.CreatedAt = time.Now()
+	}
+	type adapt Note
+	return bson.Marshal((*adapt)(nt))
+}
+
+func (nt *Note) setFromServiceNote(serviceNote calc.Note) {
+	if serviceNote.Title != nil {
+		nt.Title = *serviceNote.Title
+	}
+	if serviceNote.Body != nil {
+		nt.Body = *serviceNote.Body
+	}
+}
+
+func GetNewNote(payloadData calc.CreateNotePayload) (retVal Note) {
+	retVal.UserID = payloadData.UserID
+	if payloadData.Note != nil {
+		retVal.setFromServiceNote(*payloadData.Note)
+	}
+	return
+}
