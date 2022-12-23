ff9514e07bf2cc12d83e2d88d50e4f549281ddbb
Jawad Ahmed Khan <42826824+jawad-kp@users.noreply.github.com>
2022-12-23 19:56:45 +0530

    feature(mongo_client, db_client): added functionality to fetch notes from db

--- db/base_db.go
+++ db/base_db.go
@@ -7,4 +7,6 @@ import (
 
 type DB interface {
 	SaveNoteToDb(ctx context.Context, data *models.Note) (resp models.Note, err error)
+	GetNoteByUUID(ctx context.Context, uuid string) (resp models.Note, err error)
+	GetNoteByUserID(ctx context.Context, userID string) (resp []models.Note, err error)
 }
--- db/mongo_client.go
+++ db/mongo_client.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"goa-test/models"
 )
@@ -17,5 +18,24 @@ func NewMongoService(collection *mongo.Collection) *MongoService {
 func (m MongoService) SaveNoteToDb(ctx context.Context, data *models.Note) (resp models.Note, err error) {
 	_, err = m.collection.InsertOne(ctx, data)
 	resp = *data
+	return
+}
+
+func (m MongoService) GetNoteByUserID(ctx context.Context, userID string) (resp []models.Note, err error) {
+	var data *mongo.Cursor
+	data, err = m.collection.Find(ctx, bson.D{{"user_id", userID}})
+	if err != nil {
+		return
+	}
+	if data == nil {
+		return
+	}
+	resp = make([]models.Note, 0)
+	err = data.All(ctx, &resp)
+	return
+}
+
+func (m MongoService) GetNoteByUUID(ctx context.Context, uuid string) (resp models.Note, err error) {
+	err = m.collection.FindOne(ctx, bson.D{{"uuid", uuid}}).Decode(&resp)
 	return
 }
