80bedaded431ce21b7b84a95683c74ef2e6f3dad
Jawad Ahmed Khan <42826824+jawad-kp@users.noreply.github.com>
2022-12-23 22:11:51 +0530

    feat(db): now includes compatability with soft delete

--- db/base_db.go
+++ db/base_db.go
@@ -9,4 +9,5 @@ type DB interface {
 	SaveNoteToDb(ctx context.Context, data *models.Note) (resp models.Note, err error)
 	GetNoteByUUID(ctx context.Context, uuid string) (resp models.Note, err error)
 	GetNoteByUserID(ctx context.Context, userID string) (resp []models.Note, err error)
+	SoftDeleteNoteByUUID(ctx context.Context, uuid string) (err error)
 }
--- db/mongo_client.go
+++ db/mongo_client.go
@@ -2,9 +2,12 @@ package db
 
 import (
 	"context"
+	"errors"
+	"goa-test/models"
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"goa-test/models"
 )
 
 type MongoService struct {
@@ -23,7 +26,11 @@ func (m MongoService) SaveNoteToDb(ctx context.Context, data *models.Note) (resp
 
 func (m MongoService) GetNoteByUserID(ctx context.Context, userID string) (resp []models.Note, err error) {
 	var data *mongo.Cursor
-	data, err = m.collection.Find(ctx, bson.D{{"user_id", userID}})
+	filter := bson.M{
+		"user_id":    userID,
+		"deleted_at": bson.M{"$eq": time.Time{}},
+	}
+	data, err = m.collection.Find(ctx, filter)
 	if err != nil {
 		return
 	}
@@ -36,6 +43,27 @@ func (m MongoService) GetNoteByUserID(ctx context.Context, userID string) (resp 
 }
 
 func (m MongoService) GetNoteByUUID(ctx context.Context, uuid string) (resp models.Note, err error) {
-	err = m.collection.FindOne(ctx, bson.D{{"uuid", uuid}}).Decode(&resp)
+	filter := bson.M{
+		"uuid":       uuid,
+		"deleted_at": bson.M{"$eq": time.Time{}},
+	}
+	err = m.collection.FindOne(ctx, filter).Decode(&resp)
+	return
+}
+
+func (m MongoService) SoftDeleteNoteByUUID(ctx context.Context, uuid string) (
+	err error) {
+	filter := bson.M{
+		"uuid":       uuid,
+		"deleted_at": bson.M{"$eq": time.Time{}},
+	}
+	update := bson.D{{Key: "$set", Value: bson.D{{Key: "deleted_at", Value: time.Now()}}}}
+	res, err := m.collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return
+	}
+	if res.ModifiedCount == 0 {
+		return errors.New("document not found")
+	}
 	return
 }
