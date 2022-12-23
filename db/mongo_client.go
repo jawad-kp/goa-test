aa91f9e1d558f62732b2e899600fba86d719a028
Jawad Ahmed Khan <42826824+jawad-kp@users.noreply.github.com>
2022-12-23 18:21:09 +0530

    fix(mongo_client): returns updated value now

--- db/mongo_client.go
+++ db/mongo_client.go
@@ -16,5 +16,6 @@ func NewMongoService(collection *mongo.Collection) *MongoService {
 
 func (m MongoService) SaveNoteToDb(ctx context.Context, data *models.Note) (resp models.Note, err error) {
 	_, err = m.collection.InsertOne(ctx, data)
+	resp = *data
 	return
 }
